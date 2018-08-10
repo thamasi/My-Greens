09cbca4ebeecd4211d4344ce365151ee8cb3311a
thamasi <thamasi.yellow@gmail.com>
2018-08-10 14:19:06 -0700

    Added new plants

--- src/main/java/com/lrngs/springboot/cntlr/GardenController.java
+++ src/main/java/com/lrngs/springboot/cntlr/GardenController.java
@@ -17,6 +17,8 @@ public class GardenController
 	{
 		return Arrays.asList(new Plant("Avocado", "2016", "Healthy fruit", "medium", "2", "Fruit"),
 				new Plant("Lime", "2016", "Juicy Mayor Lime", "Abundance", "4", "Fruit"),
+				new Plant("Arabian Jasmine", "2016", "Fragrant", "Abundance", "5", "Flower"),
+				new Plant("Star Jasmine", "2016", "Magical Aroma", "Abundance", "4", "Flower"),
 				new Plant("Guava", "2015", "Red medium sweet guava", "Abundance", "5", "Fruit"));
 
 	}
