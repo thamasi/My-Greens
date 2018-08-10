2ecf965e62247a477d6a921ce146dd0b905953a6
thamasi <thamasi.yellow@gmail.com>
2018-08-10 14:42:20 -0700

    new plants

--- src/main/java/com/lrngs/springboot/cntlr/GardenController.java
+++ src/main/java/com/lrngs/springboot/cntlr/GardenController.java
@@ -19,7 +19,8 @@ public class GardenController
 				new Plant("Lime", "2016", "Juicy Mayor Lime", "Abundance", "4", "Fruit"),
 				new Plant("Arabian Jasmine", "2016", "Fragrant", "Abundance", "5", "Flower"),
 				new Plant("Star Jasmine", "2016", "Magical Aroma", "Abundance", "4", "Flower"),
-				new Plant("Guava", "2015", "Red medium sweet guava", "Abundance", "5", "Fruit"));
+				new Plant("Guava", "2015", "Red medium sweet guava", "Abundance", "5", "Fruit"),
+				new Plant("Jasmine Molle", "2018", "Tiny fragrant flowers", "Abundance", "5", "Flower"));
 
 	}
 
